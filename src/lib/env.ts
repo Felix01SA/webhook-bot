1c67fa836f8138a2dea3df34b482210ccda4a36b
Felix01SA <felixsouza1200@gmail.com>
2024-05-09 00:00:26 -0300

    feat: server port variable from envirenment

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -8,6 +8,7 @@ const schema = z.object({
     NODE_ENV: z.string().optional(),
     SMEE_URI: z.string().url().optional(),
     HOST_URL: z.string().url(),
+    PORT: z.string().transform((val) => Number(val)),
 })
 
 export const env = schema.parse(process.env)
