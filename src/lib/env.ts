c1a048bf2349c9ea32cf7c0bd1e57eb294c7602b
Felix01SA <felixsouza1200@gmail.com>
2024-05-08 22:17:33 -0300

    feat: new variable

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -7,6 +7,7 @@ const schema = z.object({
     CHANNEL_ID: z.string(),
     NODE_ENV: z.string().optional(),
     SMEE_URI: z.string().url().optional(),
+    HOST_URL: z.string().url(),
 })
 
 export const env = schema.parse(process.env)
