a92844e579bd4b4f270f192f71844d4ea39aa0f8
Felix01SA <felixsouza1200@gmail.com>
2024-05-09 21:01:46 -0300

    fix: variaveis nao utilizadas removidas do schema
    variavel CHANNEL_ID pois sera guardada na database

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,7 +4,6 @@ import { z } from 'zod'
 
 const schema = z.object({
     BOT_TOKEN: z.string(),
-    CHANNEL_ID: z.string(),
     NODE_ENV: z.string().optional(),
     SMEE_URI: z.string().url().optional(),
     HOST_URL: z.string().url(),
